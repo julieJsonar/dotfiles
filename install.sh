b7e0be76ffcb1ee921dc0efcb2171c865bd2cc2c
Huawen Yu <hyu@fortinet.com>
2015-11-16 17:11:29 -0800

    update: install.sh for ubuntu

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get remove mono-runtime-common
 sudo apt-get install -y audacious gnome-mplayer xfce4-notes
 
-sudo apt-get install -y curl zsh xterm
+sudo apt-get install -y curl zsh xterm tmux
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sudo apt-get install -y git subversion
@@ -17,7 +17,7 @@ sudo apt-get install -y tree cscope ctags w3m
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Develop Env
-sudo apt-get install -y ia32-libs smbclient openssh-server
+sudo apt-get install -y ia32-libs smbclient openssh-server minicom lftp meld
 
 # JDK
 sudo apt-add-repository ppa:webupd8team/java
