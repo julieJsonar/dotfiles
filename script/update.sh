fb468ca390357cc0fb66dae68ce96375656dc6ee
Huawen Yu <hyu@fortinet.com>
2017-02-13 20:45:08 -0800

    update vim plugin-logger

--- script/update.sh
+++ script/update.sh
@@ -42,7 +42,7 @@ git_repos=( \
   "$HOME/.vim/bundle/Decho" \
   "$HOME/.vim/bundle/vim-rooter" \
   "$HOME/.vim/bundle/neomake" \
-  "$HOME/.vim/bundle/color-scheme-holokai-for-vim" \
+  "$HOME/.vim/bundle/vimlogger" \
 )
 
 # functions {{{1
