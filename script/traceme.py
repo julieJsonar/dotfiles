f2fe57cadb32bbf5b13705f38d6b7487b9ddd2c2
Huawen Yu <hyu@fortinet.com>
2017-07-18 10:45:49 -0700

    fix bug of traceme.py

--- script/traceme.py
+++ script/traceme.py
@@ -7,7 +7,22 @@ import ConfigParser
 import argparse
 import re
 
+# args
+arg_action = 'add'
+arg_file_regex = 'daemon\/wad.*\.c'
+arg_func_regex = ''
+arg_client_addr = 'trace'
+arg_target_conf = 'trace-wad'
+
+ign_files = {}
+ign_functions = {}
+ign_funcregs = {}
+
 class NormalReturn(Exception):
+   """Base class for other exceptions"""
+   pass
+
+class NeovimNone(Exception):
    """Base class for other exceptions"""
    pass
 
@@ -43,8 +58,13 @@ def query_yes_no(question, default="yes"):
             sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 
+def parse_args():
+    global arg_action
+    global arg_file_regex
+    global arg_func_regex
+    global arg_client_addr
+    global arg_target_conf
 
-try:
     parser = argparse.ArgumentParser(description='Usage of ' + __file__)
     parser.add_argument('-a','--action', help='Action: add|clear', required=False)
     parser.add_argument('-f','--file',   help='File name match regex', required=False)
@@ -53,42 +73,45 @@ try:
     parser.add_argument('-t','--target', help='Target config dir name', required=False)
     args = vars(parser.parse_args())
 
-    arg_action = 'add'
     if 'action' in args and args['action'] is not None:
         arg_action = args['action']
-
-    arg_file_regex = 'daemon\/wad.*\.c'
     if 'file' in args and args['file'] is not None:
         arg_file_regex = args['file']
-
-    arg_func_regex = ''
     if 'symbol' in args and args['symbol'] is not None:
         arg_func_regex = args['symbol'];
-
-    arg_client_addr = 'trace'
     if 'client' in args and args['client'] is not None:
         arg_client_addr = args['client'];
-
-    arg_target_conf = 'trace-wad'
     if 'target' in args and args['target'] is not None:
         arg_target_conf = args['target'];
 
-    question = "Are you sure the tag is lasted by 'tagme'?"
-    choice = query_yes_no(question, default="yes")
-    if not choice:
-        raise NormalReturn
+def load_from_file(res, fn, info):
+    global arg_target_conf
 
-    start_time = time.time()
-    try:
-        # Embed neovim into your python application instead of binding to a running neovim instance.
-        #nvim = neovim.attach('child', argv=["/bin/env", "nvim", "--embed"])
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    f = dir_path + '/' + arg_target_conf + '/' + fn
+    print("Load {} and {} ...".format(f, info))
+    if os.path.isfile(f):
+        res = set(line.strip() for line in open(f))
+    return res
 
-        # Create a python API session attached to unix domain socket created above:
-        nvim = neovim.attach('socket', path='/tmp/nvim.' + arg_client_addr)
-    except Exception, e:
-        raise type(e)(e.message + ' Should existed neovim instance by' \
-                      '\n    NVIM_PYTHON_LOG_FILE=/tmp/nvim.log NVIM_PYTHON_LOG_LEVEL=DEBUG nvim'\
-                      '\n    NVIM_LISTEN_ADDRESS=/tmp/nvim.' + arg_client_addr + ' nvim')
+def load_ignores():
+    global ign_files
+    global ign_functions
+    global ign_funcregs
+
+    ign_files = load_from_file(ign_files, 'file', 'filter-out the ignore files')
+    ign_functions = load_from_file(ign_functions, 'function', 'filter-out the ignore functions')
+    ign_funcregs = load_from_file(ign_funcregs, 'funcregex', 'filter-out the ignore function regex')
+
+def parse_taglist():
+    global arg_action
+    global arg_file_regex
+    global arg_func_regex
+    global arg_client_addr
+    global arg_target_conf
+
+    if not os.path.isfile('.tagx'):
+        raise("No tagx file, tagme first.")
 
     matcher = '$2 == "function"'
     if arg_func_regex:
@@ -102,76 +125,64 @@ try:
     awk_cmdstr = "awk '(" + matcher + "){print $4\"+\"$3\" = \"$1}' .tagx >> /tmp/vim.taglist"
     print awk_cmdstr
     os.system(awk_cmdstr)
+
+# @return bool
+def filterout_trace_functions(file_line, func_name, filterout_files):
+    global ign_files
+    global ign_functions
+    global ign_funcregs
+
+    if func_name in ign_functions:
+        return True;
+
+    is_ign_func = False
+    for ign_func in ign_funcregs:
+        if not ign_func:
+            continue
+        if ign_func.startswith('#'):
+            continue
+        if re.search(ign_func, func_name):
+            is_ign_func = True
+            continue
+    if is_ign_func:
+        return True
+
+    is_ign_file = False
+    for ign_file in ign_files:
+        if not ign_file:
+            continue
+        if ign_file.startswith('#'):
+            continue
+        if re.search(ign_file, file_line[0]):
+            if file_line[0] not in filterout_files:
+                filterout_files.add(file_line[0])
+                print("ign_files: pattern={} file={}".format(ign_file, file_line[0]))
+            is_ign_file = True
+            continue
+    if is_ign_file:
+        return True
+    return False
+
+# @return f_cnt, tag_cnt, {file:{line:function, }, }
+def parse_trace_functions():
     if not os.path.isfile('/tmp/vim.taglist'):
         raise("No taglist /tmp/vim.taglist")
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    # skip files
-    ign_files = {}
-    fn = dir_path + '/' + arg_target_conf + '/file'
-    print("Load {} and filter-out the ignore files ...".format(fn))
-    if os.path.isfile(fn):
-        with open(fn) as f:
-            ign_files = Counter(f.read().split())
-
-    # skip functions
-    ign_functions = {}
-    fn = dir_path + '/' + arg_target_conf + '/function'
-    print("Load {} and filter-out the ignore functions ...".format(fn))
-    if os.path.isfile(fn):
-        with open(fn) as f:
-            ign_functions = Counter(f.read().split())
-
-    # skip functions
-    ign_funcregs = {}
-    fn = dir_path + '/' + arg_target_conf + '/funcregex'
-    print("Load {} and filter-out the ignore function regex ...".format(fn))
-    if os.path.isfile(fn):
-        with open(fn) as f:
-            ign_funcregs = Counter(f.read().split())
-
-    # {file:{line:function, }, }
+    filterout_files = set()
     dict_result = {}
-    filterout_files = {}
     item_cnt = 0
     file_cnt = 0
     with open('/tmp/vim.taglist') as fp:
         config = ConfigParser.RawConfigParser(allow_no_value=True)
         config.readfp(fp)
         for sec in config.sections():
-            for (k, func_name) in config.items(sec):
-                if arg_action == 'add' and func_name in ign_functions:
-                     continue
-
-                is_ign_func = False
-                if arg_action == 'add':
-                    for (ign_func, ign_v) in ign_funcregs.iteritems():
-                        if ign_func.startswith('#'):
-                            continue
-                        if re.search(ign_func, func_name):
-                            is_ign_func = True
-                            continue
-                if is_ign_func:
-                    continue
-
-                file_line = k.split("+")
+            for (_file_line, func_name) in config.items(sec):
+                file_line = _file_line.split("+")
                 if len(file_line) < 2:
                     print("len not enough: str={} len={}".format(str(file_line), len(file_line)))
                     continue
 
-                is_ign_file = False
-                if arg_action == 'add':
-                    for (ign_file, ign_v) in ign_files.iteritems():
-                        if ign_file.startswith('#'):
-                            continue
-                        if re.search(ign_file, file_line[0]):
-                            if file_line[0] not in filterout_files:
-                                filterout_files[file_line[0]] = 1
-                                print("ign_files: pattern={} file={}".format(ign_file, file_line[0]))
-                            is_ign_file = True
-                            continue
-                if is_ign_file:
+                if arg_action == 'add' and filterout_trace_functions(file_line, func_name, filterout_files):
                     continue
 
                 if file_line[0] not in dict_result:
@@ -180,11 +191,35 @@ try:
                 line_func = dict_result[file_line[0]]
                 line_func[file_line[1]] = func_name
                 item_cnt += 1
+    return file_cnt, item_cnt, dict_result
 
-    print("Add trace functions number {} ...".format(item_cnt))
+def get_nvim_instance(client_addr):
+    try:
+        # Embed neovim into your python application instead of binding to a running neovim instance.
+        #nvim = neovim.attach('child', argv=["/bin/env", "nvim", "--embed"])
 
+        # Create a python API session attached to unix domain socket created above:
+        path = '/tmp/nvim.' + client_addr
+        nvim = neovim.attach('socket', path='/tmp/nvim.' + client_addr)
+        return path, nvim
+    except Exception, e:
+        pass
+    return path, None
+
+# f_cnt, tag_cnt, {file:{line:function, }, }
+def add_trace_nvim(file_cnt, item_cnt, dict_result):
+    global arg_action
+    global arg_client_addr
+
+    path, nvim = get_nvim_instance(arg_client_addr)
+    if nvim is None:
+        os.system('rm -fr ' + path)
+        path, nvim = get_nvim_instance(arg_client_addr)
+    if nvim is None:
+        raise NeovimNone
+
+    # setup progress-bar
     toolbar_width = 40
-    # setup toolbar
     sys.stdout.write("[%s]" % (" " * toolbar_width))
     sys.stdout.flush()
     sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
@@ -224,24 +259,58 @@ try:
                 ibar = toolbar_width * i / file_cnt
 
         nvim.command("w")
-
     sys.stdout.write("\n")
-    end_time = time.time()
 
+def patch_trace_implement():
+    global arg_target_conf
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
     f_patch = dir_path + '/' + arg_target_conf + '/patch.diff'
     cmdstr = 'if ! patch -R -p0 --dry-run < ' + f_patch + '; then ' \
-            'patch -p0 < ' + f_patch + '; fi'
+        'patch -p0 < ' + f_patch + '; fi'
     print cmdstr
     os.system(cmdstr)
 
-    print("seconds: {}".format(end_time - start_time))
+def main():
+    global arg_client_addr
 
-except NormalReturn:
-    print "Done!"
+    try:
+        parse_args()
 
-except Exception as e:
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    print(exc_type, fname, exc_tb.tb_lineno)
-    print "  : " + str(e)
+        question = "Are you sure the tag is lasted by 'tagme'?"
+        choice = query_yes_no(question, default="yes")
+        if not choice:
+            return
 
+        start_time = time.time()
+        parse_taglist()
+        if not os.path.isfile('/tmp/vim.taglist'):
+            raise("No taglist /tmp/vim.taglist")
+
+        load_ignores()
+
+        # {file:{line:function, }, }
+        file_cnt, item_cnt, dict_result = parse_trace_functions()
+        print("Add trace functions number {} ...".format(item_cnt))
+        add_trace_nvim(file_cnt, item_cnt, dict_result)
+
+        end_time = time.time()
+        patch_trace_implement()
+        print("seconds: {}".format(end_time - start_time))
+
+    except NormalReturn:
+        print "Done!"
+    except NeovimNone:
+        print("Should existed neovim instance by" \
+              "\n    NVIM_PYTHON_LOG_FILE=/tmp/nvim.log NVIM_PYTHON_LOG_LEVEL=DEBUG nvim"\
+              "\n    NVIM_LISTEN_ADDRESS=/tmp/nvim.{} nvim".format(arg_client_addr))
+
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print "  : " + str(e)
+
+
+if  __name__ =='__main__':
+    main()
