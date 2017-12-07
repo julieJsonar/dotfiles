6b375a358f861ff67a7a57892fcc63fa750e5269
Huawen Yu <hyu@fortinet.com>
2017-12-07 15:59:14 -0800

    update wiki: add linux howtos

--- script/update.sh
+++ script/update.sh
@@ -202,7 +202,8 @@ Main ()
         fi
     done
 
-    Run "cd ~/grive/gitme; grive -s ./gitme"
+    Run "cd ~/grive/gitme"
+    Run "grive -s ./gitme"
 
     DEBUG printf "###$(basename $0):${BASH_LINENO[0]}: ${FUNCNAME[0]} {{{${#FUNCNAME[@]}\n"
 }
